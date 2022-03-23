a60305d9c60247758d487c9cb480bdee9215d4e9
Mai Anh Khoa <maikhoa000@gmail.com>
2022-03-23 23:59:07 +0700

    add cycle sort module

---
+++ src/modules/cycle_sort_family/index.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "cycle_sort.hpp"
---
+++ src/test/cycle_sort_family/cycle_sort.test.cpp
@@ -0,0 +1,37 @@
+#include "test/tester.hpp"
+#include "modules/cycle_sort_family/index.hpp"
+
+int main()
+{
+
+	Printer::print_color<colors::fg_blue>("\n[########## CYCLE SORT ##########]\n");
+
+	auto tester = Tester<mak::cycle_sort>();
+
+	std::initializer_list<int> i1 = {};
+	std::initializer_list<int> i2 = { 3 };
+	std::initializer_list<int> i3 = { 2, -9, 1, -5, 1, -5, 6, 3, 3, -1 };
+	tester.test_std_vector(i1, i2, i3);
+	tester.test_std_list(i1, i2, i3);
+	tester.test_std_deque(i1, i2, i3);
+	//tester.test_std_forward_list(i1, i2, i3);
+
+	tester.test_std_array(
+		std::array<int, 0>{},
+		std::to_array({ 3 }),
+		std::to_array({ 2, -9, 1, -5, 1, -5, 6, 3, 3, -1 })
+	);
+
+	tester.test_static_array(
+		{ 3 },
+		{ 2, -9, 1, -5, 1, -5, 6, 3, 3, -1 }
+	);
+
+	tester.test_dynamic_array<0, 1, 10>(
+		new int[] {},
+		new int[] {3},
+		new int[] {2, -9, 1, -5, 1, -5, 6, 3, 3, -1}
+	);
+
+	Printer::print_color<colors::fg_blue>("\n[#################################]\n");
+}
