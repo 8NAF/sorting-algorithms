d10b2914604164e04207032fdbed67468f5ead5a
Mai Anh Khoa <maikhoa000@gmail.com>
2022-04-11 16:24:46 +0700

    🎨 [refactor]: rename iterator_t -> range_t

--- src/concepts/tag_to_iter.concept.hpp
+++ src/concepts/tag_to_iter.concept.hpp
@@ -9,15 +9,15 @@ namespace mak
 		/**
 		 * Check the corresponding range concept based on the tag provided
 		 */
-		template <class iterator_t, class tag_t>
+		template <class range_t, class tag_t>
 		concept tag_to_range =
 			std::derived_from<tag_t, std::input_iterator_tag> &&
 			(
-				std::same_as<tag_t, std::contiguous_iterator_tag> ? std::ranges::contiguous_range<iterator_t> :
-				std::same_as<tag_t, std::random_access_iterator_tag > ? std::ranges::random_access_range<iterator_t> :
-				std::same_as<tag_t, std::bidirectional_iterator_tag> ? std::ranges::bidirectional_range<iterator_t> :
-				std::same_as<tag_t, std::forward_iterator_tag> ? std::ranges::forward_range<iterator_t> :
-				std::ranges::input_range<iterator_t>
+				std::same_as<tag_t, std::contiguous_iterator_tag> ? std::ranges::contiguous_range<range_t> :
+				std::same_as<tag_t, std::random_access_iterator_tag > ? std::ranges::random_access_range<range_t> :
+				std::same_as<tag_t, std::bidirectional_iterator_tag> ? std::ranges::bidirectional_range<range_t> :
+				std::same_as<tag_t, std::forward_iterator_tag> ? std::ranges::forward_range<range_t> :
+				std::ranges::input_range<range_t>
 			);
 
 		/**
