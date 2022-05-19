5ea079d8621bf5c735568e5a94e171f275d38ba6
Mai Anh Khoa <maikhoa000@gmail.com>
2022-05-19 15:23:12 +0700

    🔧 [fix]: update comparator & sortable concepts

--- src/concepts/comparator.concept.hpp
+++ src/concepts/comparator.concept.hpp
@@ -8,13 +8,23 @@ namespace mak
 {
 	using types::default_comparator;
 	using types::default_projection;
-	
+
 	namespace concepts
 	{
 		template <class func_t>
-		concept three_way_comparator = std::predicate<
-			func_t, std::partial_ordering
-		>;
+		concept three_way_comparator =
+			std::copy_constructible<func_t> &&
+			std::predicate<func_t&, std::partial_ordering>;
+
+		template <class func_t, class value_t>
+		concept two_way_comparator =
+			std::copy_constructible<func_t> &&
+			std::predicate<func_t&, value_t&, value_t&>;
+
+		template <class func_t, class value_t>
+		concept comparator =
+			three_way_comparator<func_t> ||
+			two_way_comparator<func_t, value_t>;
 
 		template <
 			class iterator_t,
@@ -24,13 +34,9 @@ namespace mak
 		concept sortable =
 		(
 			three_way_comparator<comparator_t> ?
-			std::sortable<
-				iterator_t,
-				default_comparator,
-				projection_t
-			>
-			&& std::three_way_comparable<
-				std::indirect_result_t<projection_t, iterator_t>
+			std::sortable<iterator_t, default_comparator, projection_t> &&
+			std::three_way_comparable<
+				typename std::projected<iterator_t, projection_t>::value_type
 			> :
 			std::sortable<iterator_t, comparator_t, projection_t>
 		);
